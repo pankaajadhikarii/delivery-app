6b4bc9d845052992122ce7a0ee5bdf3812ac40ff
pankaajadhikarii <pankaajadhikarii@gmail.com>
2025-08-16 16:14:02 +0545

    feat: user entities created

--- packages/user_repository/lib/src/entities/user_entity.dart
+++ packages/user_repository/lib/src/entities/user_entity.dart
@@ -10,12 +10,21 @@ class MyUserEntity {
       required this.name,
       required this.hasActiveCart});
 
-    Map<String, Object> toDocument () {
-      return {
-        'userId': userId,
-        'email': email,
-        'name': name,
-        'hasActiveCart': hasActiveCart
-      };
-    }
+  Map<String, Object> toDocument() {
+    return {
+      'userId': userId,
+      'email': email,
+      'name': name,
+      'hasActiveCart': hasActiveCart
+    };
+  }
+
+  static MyUserEntity fromDocument(Map<String, dynamic> doc) {
+    return MyUserEntity(
+      userId: doc['userId'],
+      email: doc['email'],
+      name: doc['name'],
+      hasActiveCart: doc['hasActiveCart'],
+    );
+  }
 }
