82aa3f2ed2b33b89f11593cb4f6a23eefd943d41
pankaajadhikarii <pankaajadhikarii@gmail.com>
2025-08-25 14:19:38 +0545

    folder structure

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -3,4 +3,5 @@ import 'dart:ui';
 class AppColor {
   static const Color primary = Color(0xff1E3A8A);
   static const Color secondary = Color(0xff1E3A8A);
+  static const Color body = Color(0xffFFFFFF);
 }
